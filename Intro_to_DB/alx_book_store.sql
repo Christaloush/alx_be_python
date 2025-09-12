673fd3c380073d2babf51482a662e5e3366e3879
Chris Selom Anani <chrisanani05@gmail.com>
2025-09-12 13:35:19 +0000

    Still fixing

--- Intro_to_DB/alx_book_store.sql
+++ Intro_to_DB/alx_book_store.sql
@@ -1,14 +1,11 @@
--- create database
 CREATE DATABASE alx_book_store;
 USE alx_book_store;
 
--- create authors table
 CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
--- create books table
 CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -18,7 +15,6 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- create customers table
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
@@ -26,7 +22,6 @@ CREATE TABLE Customers (
     address TEXT
 );
 
--- create orders table
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -34,7 +29,6 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- create order_details table
 CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
